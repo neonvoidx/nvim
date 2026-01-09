d33417430cc5b8df2ac45cfa4f42a694915f92f9
jrreed <jacob.reed@oracle.com>
2026-01-09 13:13:31 -0500

    up

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -6,27 +6,28 @@ return {
     },
     event = "InsertEnter",
     cmd = "Copilot",
-    opts = {
-      copilot_node_command = function()
-        local nixCatsUtils = require("nixCatsUtils")
-        if not nixCatsUtils.isNixCats then
-          return vim.fn.expand("$HOME") .. "/.local/share/mise/installs/node/24.12.0/bin/node"
-        end
-      end,
-      filetypes = {
-        markdown = false,
-        help = false,
-        sh = function()
-          if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), "^%.env.*") then
-            return false
-          end
-          return true
-        end,
-      },
-      nes = {
-        enabled = false,
-      },
-    },
+    opts = function()
+      local nixCatsUtils = require("nixCatsUtils")
+      local opts = {
+        filetypes = {
+          markdown = false,
+          help = false,
+          sh = function()
+            if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), "^%.env.*") then
+              return false
+            end
+            return true
+          end,
+        },
+        nes = {
+          enabled = false,
+        },
+      }
+      if not nixCatsUtils.isNixCats then
+        opts.copilot_node_command = vim.fn.expand("$HOME") .. "/.local/share/mise/installs/node/24.12.0/bin/node"
+      end
+      return opts
+    end,
   },
   { "leisurelicht/lualine-copilot.nvim" },
 }
