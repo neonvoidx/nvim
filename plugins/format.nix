0570c4550e271004b99441b487a2431259efcdeb
jrreed <jacob.reed@oracle.com>
2026-05-06 14:34:36 -0400

    up

--- plugins/format.nix
+++ plugins/format.nix
@@ -7,28 +7,40 @@
       setupOpts = {
         formatters_by_ft = {
           javascript = [
-            "eslint_d"
-            "prettierd"
+            [
+              "eslint_d"
+              "prettierd"
+            ]
           ];
           typescript = [
-            "eslint_d"
-            "prettierd"
+            [
+              "eslint_d"
+              "prettierd"
+            ]
           ];
           javascriptreact = [
-            "eslint_d"
-            "prettierd"
+            [
+              "eslint_d"
+              "prettierd"
+            ]
           ];
           typescriptreact = [
-            "eslint_d"
-            "prettierd"
+            [
+              "eslint_d"
+              "prettierd"
+            ]
           ];
           "javascript.jsx" = [
-            "eslint_d"
-            "prettierd"
+            [
+              "eslint_d"
+              "prettierd"
+            ]
           ];
           "typescript.tsx" = [
-            "eslint_d"
-            "prettierd"
+            [
+              "eslint_d"
+              "prettierd"
+            ]
           ];
           css = [ "prettierd" ];
           html = [ "prettierd" ];
@@ -61,7 +73,7 @@
             if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
               return
             end
-            return { timeout_ms = 500, lsp_format = "fallback" }
+            return { timeout_ms = 3000, lsp_format = "fallback" }
           end
         '';
       };
