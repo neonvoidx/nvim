795b8202d091d37310369032ebf2ab450ebeefdd
neonvoidx <me@neonvoid.dev>
2026-05-27 09:33:18 -0400

    up

--- lua/plugins/persistence.lua
+++ lua/plugins/persistence.lua
@@ -1,23 +1,6 @@
-local project_root = require("util").project_root()
+require("persistence").setup()
 
-require("persistence").setup({
-  main = project_root,
-})
-
-vim.schedule(function()
-  require("persistence").load()
-end)
 -- load the session for the current directory
 vim.keymap.set("n", "<leader>qs", function()
   require("persistence").load()
 end, { desc = "Load session" })
-
--- select a session to load
-vim.keymap.set("n", "<leader>qS", function()
-  require("persistence").select()
-end, { desc = "Select session" })
-
--- load the last session
-vim.keymap.set("n", "<leader>ql", function()
-  require("persistence").load({ last = true })
-end, { desc = "Load last session" })
